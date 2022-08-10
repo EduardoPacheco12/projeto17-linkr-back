96dba6e5d9d0e29456a00361266dd601d3fece4b
KadsonLima <kadson-kaio@hotmail.com>
2022-08-10 17:24:35 -0300

    fix: postsHandler

--- src/handlers/postsHandler.js
+++ src/handlers/postsHandler.js
@@ -2,20 +2,15 @@ import bcrypt from "bcrypt";
 import urlMetadata from 'url-metadata';
 
 
-async function getMetadata(post) {
-    const {url, title, image, description} = await urlMetadata(post.url)
-    
-    const metadata = { title, image, url, description};
 
-    return metadata
+ export async function postsMetadata(lista) {
 
-}
-
-
-export async function postsMetadata(lista) {
-
-  for (let i = 0; i < lista.length; i++) {
-    lista[i]["metadata"] = (await getMetadata(lista[i]));
+  try {
+   const {title, image, url, description} = await urlMetadata(lista.url);
+   lista["metadata"] = {title, image, url, description};
+  } catch (error) {
+    console.log(error)
   }
+ 
   return lista
 }

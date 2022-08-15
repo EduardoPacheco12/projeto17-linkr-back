6f435e67c3258d7fa3d1d6298b9e639e4b0087ff
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-08-15 16:41:14 -0300

    feat: return hashtag posts with the metadata

--- src/controllers/contentControllers.js
+++ src/controllers/contentControllers.js
@@ -1,10 +1,27 @@
-import { getContentData, getTrendingQuery } from "../repositories/contentRepository.js"
+import { getContentData, getTrendingQuery } from "../repositories/contentRepository.js";
+import postsMetadata from '../handlers/postsHandler.js';
+
+async function getMetadata(posts) {
+  const postsWithMetadata = [];
+
+  for(const post of posts) {
+    const postMetadata = await postsMetadata(post.url);
+    const {title, image, description, url} = postMetadata;
+    postsWithMetadata.push(
+      {...post, metadata:{title, image, description, url}}
+    );
+  }
+
+  return postsWithMetadata;
+}
+
 
 export async function redirectToHashtag(req, res) {
   try {
     const data = req.params;
     const { rows: response } = await getContentData(data.id)
-    res.status(200).send(response);
+    const postsWithMetadata = await getMetadata(response);
+    res.status(200).send(postsWithMetadata);
     return;
   } catch (err) {
     console.log(err);
